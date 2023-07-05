9e595d2cf0b57670fcea55af387473ca52ab2fd6
mohammad <seyedmamad86@gmail.com>
2023-07-05 08:36:39 -0700

    add recent post and category to sidebar

--- home_app/views.py
+++ home_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from blog_app.models import Category, Post
+
+
 def home(request):
     return render(request, 'home_app/index.html')
 
+
 def sidebar(request):
-    return render(request, 'includes/sidebar.html')
+    categories = Category.objects.all()
+    recent_posts = Post.objects.order_by('-created_at')[:3]
+    return render(request, 'includes/sidebar.html', {'categories': categories, 'recent_posts': recent_posts})
