f9f74c92ae3413da41b6ac5c464e91063ddb035c
mohammad <seyedmamad86@gmail.com>
2023-07-05 08:15:10 -0700

    add like,comment and category model

--- blog_app/models.py
+++ blog_app/models.py
@@ -3,8 +3,17 @@ from account_app.models import User
 from ckeditor_uploader.fields import RichTextUploadingField
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
     title = models.CharField(max_length=505)
     title_site = models.CharField(max_length=80)
     site_meta = models.CharField(max_length=500)
@@ -19,3 +28,23 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.post.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+
+    def __str__(self):
+        return self.message[:30]
