8495b04a5e193e3ba3094de4ff5c7daff78cf9a5
mohammad <seyedmamad86@gmail.com>
2023-07-05 03:51:55 -0700

    includes header,footer and render sidebar with render partial

--- home_app/views.py
+++ home_app/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(request, 'home_app/index.html')
+
+def sidebar(request):
+    return render(request, 'includes/sidebar.html')
